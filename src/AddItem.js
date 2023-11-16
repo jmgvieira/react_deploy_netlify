83f8335b6d4b13429d29220ebc7329822b3b489e
Jorge Vieira <jmgvieira@yahoo.com>
2023-11-16 19:57:21 +0000

    corrections

--- src/AddItem.js
+++ src/AddItem.js
@@ -1,63 +1,31 @@
-import Header from './Header';
-import SearchItem from './SearchItem';
-import AddItem from './AddItem';
-import Content from './Content';
-import Footer from './Footer';
-import { useState, useEffect } from 'react';
+import { FaPlus } from 'react-icons/fa';
+import { useRef } from 'react';
 
-function App() {
-  const [items, setItems] = useState(JSON.parse(localStorage.getItem('shoppinglist')) || []);
-  const [newItem, setNewItem] = useState('')
-  const [search, setSearch] = useState('')
+const AddItem = ({ newItem, setNewItem, handleSubmit }) => {
+    const inputRef = useRef();
 
-  useEffect(() => {
-    localStorage.setItem('shoppinglist', JSON.stringify(items));
-  }, [items])
-
-  const addItem = (item) => {
-    const id = items.length ? items[items.length - 1].id + 1 : 1;
-    const myNewItem = { id, checked: false, item };
-    const listItems = [...items, myNewItem];
-    setItems(listItems);
-  }
-
-  const handleCheck = (id) => {
-    const listItems = items.map((item) => item.id === id ? { ...item, checked: !item.checked } : item);
-    setItems(listItems);
-  }
-
-  const handleDelete = (id) => {
-    const listItems = items.filter((item) => item.id !== id);
-    setItems(listItems);
-  }
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    if (!newItem) return;
-    addItem(newItem);
-    setNewItem('');
-  }
-
-  return (
-    <div className="App">
-      <Header title="Grocery List" />
-      <AddItem
-        newItem={newItem}
-        setNewItem={setNewItem}
-        handleSubmit={handleSubmit}
-      />
-      <SearchItem
-        search={search}
-        setSearch={setSearch}
-      />
-      <Content
-        items={items.filter(item => ((item.item).toLowerCase()).includes(search.toLowerCase()))}
-        handleCheck={handleCheck}
-        handleDelete={handleDelete}
-      />
-      <Footer length={items.length} />
-    </div>
-  );
+    return (
+        <form className='addForm' onSubmit={handleSubmit}>
+            <label htmlFor='addItem'>Add Item</label>
+            <input
+                autoFocus
+                ref={inputRef}
+                id='addItem'
+                type='text'
+                placeholder='Add Item'
+                required
+                value={newItem}
+                onChange={(e) => setNewItem(e.target.value)}
+            />
+            <button
+                type='submit'
+                aria-label='Add Item'
+                onClick={() => inputRef.current.focus()}
+            >
+                <FaPlus />
+            </button>
+        </form>
+    )
 }
 
-export default App;
+export default AddItem
--- src/Footer.js
+++ src/Footer.js
@@ -1,31 +1,9 @@
-import { FaPlus } from 'react-icons/fa';
-import { useRef } from 'react';
-
-const AddItem = ({ newItem, setNewItem, handleSubmit }) => {
-    const inputRef = useRef();
-
+const Footer = ({ length }) => {
     return (
-        <form className='addForm' onSubmit={handleSubmit}>
-            <label htmlFor='addItem'>Add Item</label>
-            <input
-                autoFocus
-                ref={inputRef}
-                id='addItem'
-                type='text'
-                placeholder='Add Item'
-                required
-                value={newItem}
-                onChange={(e) => setNewItem(e.target.value)}
-            />
-            <button
-                type='submit'
-                aria-label='Add Item'
-                onClick={() => inputRef.current.focus()}
-            >
-                <FaPlus />
-            </button>
-        </form>
+        <footer>
+            <p>{length} List {length === 1 ? "item" : "items"}</p>
+        </footer>
     )
 }
 
-export default AddItem
+export default Footer
